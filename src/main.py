82d151e6efcfd8f4821326acd22805add8612a2b
LuchnikKek <ilua.Kabeshov01@yandex.ru>
2024-01-23 00:10:44 +0300

    Реализована самогенерирующаяся дока Добавлены нужные зависимости Создан логгер

---
+++ src/main.py
@@ -0,0 +1,27 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+
+from src.core import config
+
+app = FastAPI(
+    # Название проекта, будет отображаться в документации
+    title=config.PROJECT_NAME,
+    # Адрес документации в красивом интерфейсе
+    docs_url='/api/openapi',
+    # Адрес документации в формате OpenAPI
+    openapi_url='/api/openapi.json',
+    # Замена стандартного JSON-сериализатора на более шуструю версию, написанную на Rust
+    default_response_class=ORJSONResponse,
+)
+
+if __name__ == '__main__':
+    # Приложение может запускаться командой
+    # `uvicorn main:app --host 0.0.0.0 --port 8000`
+    # но чтобы не терять возможность использовать дебагер,
+    # запустим uvicorn сервер через python
+    uvicorn.run(
+        'main:app',
+        host='0.0.0.0',
+        port=8000,
+    )
